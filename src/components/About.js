3205a81f80ad2756537d90e0b0f9a0c3c35a1b40
Andrijaj01 <andrijajovanovic001@gmail.com>
2021-03-11 18:33:40 +0100

    cache with localStorage

--- src/components/About.js
+++ src/components/About.js
@@ -2,25 +2,23 @@ import {useState,useEffect} from 'react'
 
 
 function About() {
-
   const [data,setData]= useState({});
-  const [title,setTitle]=useState(null);
-  const [desc,setDesc]=useState(null);
+  const [title,setTitle]=useState(localStorage.getItem('title') || '');
+  const [desc,setDesc]=useState(localStorage.getItem('description') || '');
   const get_data = async () =>{
-    //const response= await fetch('http://127.0.0.1:8000/about');
-    const response= await fetch('https://fastapibackend.herokuapp.com/about');
+    const response= await fetch('http://127.0.0.1:8000/about');
+    //const response= await fetch('https://fastapibackend.herokuapp.com/about');
     const data= await response.json();
     const title= data.title;
     const desc= data.description;
     setDesc(desc);
     setTitle(title);
-
   }
-
-    useEffect(()=>{
+  useEffect(()=>{
       get_data();
+
     }
-      ,[]
+              ,[title,desc]
     );
 
   return (
