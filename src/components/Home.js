3af53cce973602777cdc980df4deb944326c027a
Andrijaj01 <andrijajovanovic001@gmail.com>
2021-03-12 17:48:19 +0100

    typo

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,8 +6,8 @@ function About() {
   const [text,setText]=useState(localStorage.getItem('text') || '');
   const [name,setName]=useState(localStorage.getItem('name') || '');
   const get_data = async () =>{
-    //const response= await fetch('http://127.0.0.1:8000/');
-    const response= await fetch('https://fastapibackend.herokuapp.com/about');
+   // const response= await fetch('http://127.0.0.1:8000/');
+   const response= await fetch('https://fastapibackend.herokuapp.com/);
     const data= await response.json();
     const name= data.name;
     const text= data.text;
@@ -17,28 +17,28 @@ function About() {
     if(localStorage.getItem('text')!==text){
       localStorage.setItem('text','')
     }
-    if(localStorage.getItem('name')!==name){
-      localStorage.setItem('name','')
+    if(localStorage.getItem('description')!==name){
+      localStorage.setItem('description','')
     }
     
   }
   useEffect(()=>{
     get_data();
 
-    localStorage.setItem('name',name)
-    localStorage.setItem('text',text)
+    localStorage.setItem('title',title)
+    localStorage.setItem('description',desc)
 
     }
-              ,[name,text]
+              ,[title,desc]
     );
 
   return (
 
     <header className="App-header">
 	  <img src={profile}/>
-      <h1>{name}</h1>
+      <h1>{title}</h1>
       <p className='t-justify'>
-      {text}
+      {desc}
       </p>
     </header>
   );
